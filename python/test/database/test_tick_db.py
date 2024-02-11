fafcb60747a1298d95f9df2e1271ee879757de85
javifalces <javifalces@gmail.com>
2024-02-11 09:22:09 +0100

    fix test

--- python/test/database/test_tick_db.py
+++ python/test/database/test_tick_db.py
@@ -2,11 +2,16 @@ import unittest
 from database.tick_db import TickDB
 import datetime
 
+from test.utils.test_util import set_test_data_path
+
 
 class TestTickDb(unittest.TestCase):
-    tick = TickDB()
+    # set env variables LAMBDA_DATA_PATH to current test folder in data ddbb
+    data_path = set_test_data_path()
+    tick = TickDB(root_db_path=data_path)
+
     start_date = datetime.datetime(day=6, month=9, year=2021)
-    end_date = datetime.datetime(day=16, month=9, year=2021)
+    end_date = datetime.datetime(day=9, month=9, year=2021)
     instrument_test = 'eurusd_darwinex'
 
     def test_get_depth(self):
@@ -27,19 +32,22 @@ class TestTickDb(unittest.TestCase):
         )
         self.assertIsNotNone(candles_midprice)
 
+    @unittest.skip("slow not needed")
     def test_get_depth_fastparquet(self):
         import pandas as pd
 
+        start_date = self.start_date
+        end_date = self.end_date
         depth_df = self.tick.get_depth(
             instrument_pk=self.instrument_test,
-            start_date=self.start_date,
-            end_date=self.end_date,
+            start_date=start_date,
+            end_date=end_date,
         )
 
-        depth_df2 = self.tick._get_depth_pandas(
+        depth_df2 = self.tick._get_manual_pandas(
             instrument_pk=self.instrument_test,
-            start_date=self.start_date,
-            end_date=self.end_date,
+            start_date=start_date,
+            end_date=end_date,
         )
         depth_df2['date'] = pd.to_datetime(depth_df2.index * 1000000)
         depth_df2.set_index('date', inplace=True)
@@ -50,5 +58,33 @@ class TestTickDb(unittest.TestCase):
         self.assertIsNotNone(depth_df)
         self.assertIsNotNone(depth_df2)
 
-        self.assertEquals(depth_df.shape[0], depth_df2.shape[0])
-        self.assertEquals(depth_df.shape[1], depth_df2.shape[1])
+        self.assertEqual(depth_df.shape[0], depth_df2.shape[0])
+        self.assertEqual(depth_df.shape[1], depth_df2.shape[1])
+
+    @unittest.skip("slow not needed")
+    def test_get_candles_fastparquet(self):
+        import pandas as pd
+
+        instrument_test = 'eurusd_darwinex'
+        start_date = self.start_date
+        end_date = self.end_date
+        candle_df = self.tick.get_candles_midprice_time(
+            instrument_pk=instrument_test,
+            start_date=start_date,
+            end_date=end_date,
+        )
+
+        candle_df2 = self.tick._get_manual_pandas(
+            instrument_pk=instrument_test,
+            start_date=start_date,
+            end_date=end_date,
+            type_data='candle_midpricetime_MIN1',
+        )
+        candle_df2.drop_duplicates(inplace=True)
+        candle_df2['date'] = pd.to_datetime(candle_df2.index)
+
+        self.assertIsNotNone(candle_df)
+        self.assertIsNotNone(candle_df2)
+
+        self.assertEqual(candle_df.shape[0], candle_df2.shape[0])
+        self.assertEqual(candle_df.shape[1], candle_df2.shape[1])
